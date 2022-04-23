0fb693f951fbda8d3aed72a598902eca1b3b0b61
Abhishek Pal Singh <abhishekpal.singh@klearnow.com>
2022-04-23 18:22:53 +0530

    Added Bullet Points logs

--- src/test/java/interviewsampleframework/tests/ExecuteSampleTest.java
+++ src/test/java/interviewsampleframework/tests/ExecuteSampleTest.java
@@ -25,7 +25,7 @@ public class ExecuteSampleTest {
         String baseUrl = YamlReader.getData("base_url");
         dsl.launchApplication(baseUrl);
         Assert.assertEquals(baseUrl, dsl.getCurrentURL(), "Unable to launch Amazon website");
-        Reporter.log("Successfully Launched Amazon Website");
+        Reporter.log("Successfully Launched Amazon Website", true);
 
     }
 
@@ -33,7 +33,7 @@ public class ExecuteSampleTest {
     public void TestStep02_ClickOnHamburger() {
         dsl.samplePage.clickOnHamburger();
         Assert.assertTrue(dsl.samplePage.verifyHamburgerMenuExpand(), "Unable to expand Hamburger Menu");
-        Reporter.log("Successfully Expand Hamburger Menu");
+        Reporter.log("Successfully Expand Hamburger Menu", true);
     }
 
     @Test(groups = {"desktop"}, dependsOnMethods = "TestStep02_ClickOnHamburger")
@@ -41,7 +41,7 @@ public class ExecuteSampleTest {
         String hamburgerOption = YamlReader.getData("hamburgerOption");
         dsl.samplePage.selectHamburgerOption(hamburgerOption);
         Assert.assertTrue(dsl.samplePage.verifyHamburgerOptionSelected(), "Unable to click on " + hamburgerOption + " option");
-        Reporter.log("Successfully selected " + hamburgerOption + " option");
+        Reporter.log("Successfully selected " + hamburgerOption + " option", true);
     }
 
     @Test(groups = {"desktop"}, dependsOnMethods = "TestStep03_SelectHamburgerOption")
@@ -49,6 +49,8 @@ public class ExecuteSampleTest {
         dsl.samplePage.selectTelevisonOption();
         String currentUrL = dsl.getCurrentURL();
         Assert.assertTrue(dsl.samplePage.verifyTelevisonOptionSelected(currentUrL), "Unable to click on Television option");
+        Reporter.log("Successfully selected Television option", true);
+
     }
 
     @Test(groups = {"desktop"}, dependsOnMethods = "TestStep04_SelectTelevisionOption")
@@ -56,6 +58,8 @@ public class ExecuteSampleTest {
         dsl.samplePage.selectSamsungTelevisonBrand();
         String currentUrL = dsl.getCurrentURL();
         Assert.assertTrue(dsl.samplePage.verifySamsungTelevisonBrandSelected(currentUrL), "Unable to select samsung Television brand");
+        Reporter.log("Successfully selected Samsung brand", true);
+
     }
 
     @Test(groups = {"desktop"}, dependsOnMethods = "TestStep05_SelectSamsungTelevisionBrand")
@@ -63,12 +67,15 @@ public class ExecuteSampleTest {
         dsl.samplePage.SortProductResults();
         String currentUrL = dsl.getCurrentURL();
         Assert.assertTrue(dsl.samplePage.verifySortProductResults(currentUrL), "Unable to sort products results in 'high to low' ");
+        Reporter.log("Successfully sort high to low results", true);
+
     }
 
     @Test(groups = {"desktop"}, dependsOnMethods = "TestStep05_SortProductResults")
     public void TestStep06_SelectSecondProduct() {
         dsl.samplePage.selectSecondProduct();
         Assert.assertTrue(dsl.samplePage.verifyProductSelected(), "Unable to select product");
+        Reporter.log("Successfully selected second Television from results", true);
     }
 
     @Test(groups = {"desktop"}, dependsOnMethods = "TestStep05_SortProductResults")
@@ -78,6 +85,12 @@ public class ExecuteSampleTest {
         String Point_3 = YamlReader.getData("About_This_item.point_3");
         String Point_4 = YamlReader.getData("About_This_item.point_4");
         String Point_5 = YamlReader.getData("About_This_item.point_5");
+
+        Reporter.log("Point 1 : " + dsl.samplePage.getPoint1Details(), true);
+        Reporter.log("Point 2 : " + dsl.samplePage.getPoint2Details(), true);
+        Reporter.log("Point 3 : " + dsl.samplePage.getPoint3Details(), true);
+        Reporter.log("Point 4 : " + dsl.samplePage.getPoint4Details(), true);
+        Reporter.log("Point 5 : " + dsl.samplePage.getPoint5Details(), true);
 
         Assert.assertEquals(Point_1, dsl.samplePage.getPoint1Details(), "Point 1 Mismatch");
         Assert.assertEquals(Point_2, dsl.samplePage.getPoint2Details(), "Point 2 Mismatch");
@@ -89,7 +102,7 @@ public class ExecuteSampleTest {
 
     @AfterClass(groups = {"desktop"})
     public void stop_test_session() {
-//		 dsl.closeBrowserSession();
+        dsl.closeBrowserSession();
         Reporter.log("User closes the application");
     }
 
--- target/test-classes/PageObjectRepository/Desktop/QA/interviewsampleframework/SamplePage.spec
+++ target/test-classes/PageObjectRepository/Desktop/QA/interviewsampleframework/SamplePage.spec
@@ -2,5 +2,18 @@ Page Title: interviewsampleframework
 
 #Object Definitions
 ====================================================================================
+icon_hamburger                              id                              nav-hamburger-menu
+option_hamburger                            xpath                           //a[@class="hmenu-item"]/div[text()='${option}']
+Header_hamburger                            xpath                           //div[@id="hmenu-customer-name"]/b[text()="Hello, Sign in"]
+icon_main_menu                              xpath                           //ul[contains(@class,"hmenu-visible")]//div[text()="main menu"]/i
+option_televison                            xpath                           //a[text()="Televisions"]
+checkbox_samsung                            xpath                           //div[@id="s-refinements"]//span[text()="Samsung"]/ancestor::a
+dropdown_sortby                             css                             [data-action='a-dropdown-button']
+option_sortby_dropdown                      xpath                           //a[text()="Price: High to Low"]
+results_second_product                      xpath                           (//span[@data-component-type="s-search-results"]//div[contains(@class,"s-main-slot s-result-list")]/div)[3]
+section_buy                                 xpath                           //div[@id="buyBoxAccordion"]
+input_search                                css                             #twotabsearchtextbox
+header                                      tagName                         header
+list_bullets_points                         xpath                           //div[@id="feature-bullets"]//li//span[@class="a-list-item"]
 
                   
